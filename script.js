a26f15c386abb492adbba03c4bb1d2ec1cbbfa72
algorman <32680191+algorman@users.noreply.github.com>
2020-05-28 02:40:33 -0400

    did stuff and messed up
    a

--- script.js
+++ script.js
@@ -11,10 +11,45 @@ document.getElementById("sendMessage").addEventListener("click", send);
 //The message information is passed to you in the variable 'message'
 function recieveMessage(message) {
 console.log(message);
+// let messages = [
+//   { Name: "message.name", Message: "message.content"},
+// ];
+
+// function generateTableHead(table) {
+//   let thead = table.createTHead();
+//   let row = thead.insertRow();
+//   for (let key of data) {
+//     let th = document.createElement("th");
+//     let text = document.createTextNode(key);
+//     th.appendChild(text);
+//     row.appendChild(th);
+//   }
+// }
+
+
+
+// function generateTable(table, data) {
+//   for (let element of data) {
+//     let row = table.insertRow();
+//     for (key in element) {
+//       let cell = row.insertCell();
+//       let text = document.createTextNode(element[key]);
+//       cell.appendChild(text);
+//     }
+//   }
+// }
+
+// let table = document.getElementById("table");
+// let data = Object.keys(messages[0]);
+// generateTableHead(table, data);
+// generateTable(table, messages);
+
+
+
 var m = document.getElementById("Messages");
 // m.innerHTML = message.content;
 var p = document.createElement("p");
-p.innerHTML = message.content + " - " + message.name;
+p.innerHTML = message.name+ " says: " + message.content;
 m.prepend(p);
 }
 
